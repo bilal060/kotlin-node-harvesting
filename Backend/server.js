96b7fc2fb8698052a32fe108c30e863a30b19c12
maee-crypto <maee2991@gmail.com>
2025-08-11 00:42:33 +0400

    changes

--- Backend/server.js
+++ Backend/server.js
@@ -63,7 +63,12 @@ const PORT = config.server.port;
 connectDB();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true
+}));
 
 // Enhanced JSON parsing middleware with error recovery
 app.use((req, res, next) => {
